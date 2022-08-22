4a00497544679f4bc70d66028dbd609673914bda
David Lambert <david.lambert@my.utsa.edu>
2022-08-22 10:43:48 -0500

    added middleware function for body parsing

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express')
+    //module for middleware
+const bodyParser = require('body-parser')
     //required for express, app is what manipulate
 const app = express()
+    //all middleware functions use this bodyParser, auto doesnt apply to get request
+app.use(bodyParser.urlencoded({ extended: true }))
     //route handler, request and response objects, .get means wait for get request and path of '/'
 app.get('/', (req, res) => {
-    res.send("hi there")
+        res.send(`
+    <div>
+        <form method="POST">
+            <input name="email" placeholder="email" />
+            <input name="password"placeholder="password" />
+            <input name="passwordConfirmation"placeholder="password confirmation" />
+            <button>Sign Up</button>
+        </form>
+    </div>
+    `)
+    })
+    //middleware function= modifies request before sending it, next is callback func from express
+
+//run bodyparser library middleware first, when next is called it returns here and info in req.body
+app.post('/', (req, res) => {
+    //similiar to addeventlistener, but waiting for data event now. data obj in buffer in hex, sends in chunks if large
+
+    res.send("Account created")
 })
 
 //listen on port 3000 for request then run callback
