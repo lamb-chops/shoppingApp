aced3da07b4523f74f8a1c9533f871c86a562103
David Lambert <david.lambert@my.utsa.edu>
2022-08-23 15:50:13 -0500

    formatted code

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require('express')
-    //module for middleware
-const bodyParser = require('body-parser')
-const cookieSession = require('cookie-session')
-    //required for express, app is what manipulate
-const app = express()
-const usersRepo = require("./repositories/users")
-    //all middleware functions use this bodyParser, auto doesnt apply to get request
-app.use(bodyParser.urlencoded({ extended: true }))
-    //npm package to create cookie, keys property used to encrypt cookie, adds property to req object (req.session)
-app.use(cookieSession({ keys: ['asdfjkl'] }))
+const express = require("express");
+//module for middleware
+const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
+//required for express, app is what manipulate
+const app = express();
+const usersRepo = require("./repositories/users");
+//all middleware functions use this bodyParser, auto doesnt apply to get request
+app.use(bodyParser.urlencoded({ extended: true }));
+//npm package to create cookie, keys property used to encrypt cookie, adds property to req object (req.session)
+app.use(cookieSession({ keys: ["asdfjkl"] }));
 
 //route handler, request and response objects, .get means wait for get request and path of '/'
-app.get('/', (req, res) => {
-    res.send(`
+app.get("/", (req, res) => {
+  res.send(`
     <div>
     Your ID is: ${req.session.userId}
         <form method="POST">
@@ -22,26 +22,26 @@ app.get('/', (req, res) => {
             <button>Sign Up</button>
         </form>
     </div>
-    `)
-})
+    `);
+});
 
-app.post('/', async(req, res) => {
-    const { email, password, passwordConfirmation } = req.body
-        //check if someone signed up with email before
-    const existingUser = await usersRepo.getOneBy({ email: email })
-    if (existingUser) {
-        return res.send("Email in use.")
-    }
-    if (password !== passwordConfirmation) {
-        return res.send("Passwords must match.")
-    }
+app.post("/", async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+  //check if someone signed up with email before
+  const existingUser = await usersRepo.getOneBy({ email: email });
+  if (existingUser) {
+    return res.send("Email in use.");
+  }
+  if (password !== passwordConfirmation) {
+    return res.send("Passwords must match.");
+  }
 
-    const user = await usersRepo.create({ email: email, password: password })
-    req.session.userId = user.id //added by cookie-session, attached auto to req
-    res.send("Account created")
-})
+  const user = await usersRepo.create({ email: email, password: password });
+  req.session.userId = user.id; //added by cookie-session, attached auto to req
+  res.send("Account created");
+});
 
 //listen on port 3000 for request then run callback
 app.listen(3000, () => {
-    console.log("listening")
-})
+  console.log("listening");
+});
--- repositories/users.js
+++ repositories/users.js
@@ -1,89 +1,94 @@
-const fs = require('fs')
-const crypto = require('crypto')
+const fs = require("fs");
+const crypto = require("crypto");
 
 class UsersRepository {
-    constructor(filename) {
-        if (!filename) {
-            throw new Error("Creating a reposity requires a filename.")
+  constructor(filename) {
+    if (!filename) {
+      throw new Error("Creating a reposity requires a filename.");
+    }
+
+    this.filename = filename;
+    try {
+      //synchronous access, ok cuz we are just running one only once. no call back cuz not async
+      fs.accessSync(this.filename);
+    } catch (err) {
+      //creates file if does not exist, returns empty array so array always inside file
+      fs.writeFileSync(this.filename, "[]");
+    }
+  }
+  async getAll() {
+    const contents = await fs.promises.readFile(this.filename, {
+      encoding: "utf8",
+    });
+    const data = JSON.parse(contents);
+    return data;
+  }
+
+  async create(attrs) {
+    attrs.id = this.randomId();
+    const records = await this.getAll();
+    records.push(attrs);
+    await this.writeAll(records);
+
+    return attrs;
+  }
+
+  async writeAll(records) {
+    //stringify goes from string to json, null and 2 as params make json more readible instead of one line
+    //null is for custom formatter, 2 is level of indentation to use for string and nesting
+    await fs.promises.writeFile(
+      this.filename,
+      JSON.stringify(records, null, 2)
+    );
+  }
+
+  randomId() {
+    //creates random 4 bytes turned to string for unique ID
+    return crypto.randomBytes(4).toString("hex");
+  }
+
+  async getOne(id) {
+    const records = await this.getAll();
+    //find returns first record that meets conditions
+    return records.find((record) => record.id === id);
+  }
+
+  async delete(id) {
+    const records = await this.getAll();
+    //filter only returns ones that are true
+    const filteredRecords = records.filter((record) => record.id !== id);
+    await this.writeAll(filteredRecords);
+  }
+
+  async update(id, attrs) {
+    const records = await this.getAll();
+    const record = records.find((record) => record.id === id);
+    if (!record) {
+      throw new Error(`Record with id ${id} is not found.`);
+    }
+    //copies all of attrs one by one into record
+    Object.assign(record, attrs);
+    await this.writeAll(records);
+  }
+
+  async getOneBy(filters) {
+    const records = await this.getAll();
+    //for of = arrays and iterables
+    for (let record of records) {
+      let found = true;
+      //for in = iter through object, recieve every key in object
+      for (let key in filters) {
+        if (record[key] !== filters[key]) {
+          found = false;
         }
+      }
 
-        this.filename = filename
-        try {
-            //synchronous access, ok cuz we are just running one only once. no call back cuz not async
-            fs.accessSync(this.filename)
-        } catch (err) {
-            //creates file if does not exist, returns empty array so array always inside file
-            fs.writeFileSync(this.filename, '[]')
-        }
+      if (found) {
+        return record;
+      }
     }
-    async getAll() {
-        const contents = await fs.promises.readFile(this.filename, { encoding: 'utf8' })
-        const data = JSON.parse(contents)
-        return data
-    }
-
-    async create(attrs) {
-        attrs.id = this.randomId()
-        const records = await this.getAll()
-        records.push(attrs)
-        await this.writeAll(records)
-
-        return attrs
-    }
-
-    async writeAll(records) {
-        //stringify goes from string to json, null and 2 as params make json more readible instead of one line
-        //null is for custom formatter, 2 is level of indentation to use for string and nesting
-        await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2))
-    }
-
-    randomId() {
-        //creates random 4 bytes turned to string for unique ID
-        return crypto.randomBytes(4).toString('hex')
-    }
-
-    async getOne(id) {
-        const records = await this.getAll()
-            //find returns first record that meets conditions
-        return records.find(record => record.id === id)
-    }
-
-    async delete(id) {
-        const records = await this.getAll()
-            //filter only returns ones that are true
-        const filteredRecords = records.filter(record => record.id !== id)
-        await this.writeAll(filteredRecords)
-    }
-
-    async update(id, attrs) {
-        const records = await this.getAll()
-        const record = records.find(record => record.id === id)
-        if (!record) {
-            throw new Error(`Record with id ${id} is not found.`)
-        }
-        //copies all of attrs one by one into record
-        Object.assign(record, attrs)
-        await this.writeAll(records)
-    }
-
-    async getOneBy(filters) {
-        const records = await this.getAll()
-            //for of = arrays and iterables
-        for (let record of records) {
-            let found = true
-                //for in = iter through object, recieve every key in object
-            for (let key in filters) {
-                if (record[key] !== filters[key]) {
-                    found = false
-                }
-            }
-
-            if (found) {
-                return record
-            }
-        }
-    }
+  }
 }
 //recieve instance of class instead of just passing class then instantiating when called
 //works cuz only need one
-module.exports = new UsersRepository('users.json')
+module.exports = new UsersRepository("users.json");
