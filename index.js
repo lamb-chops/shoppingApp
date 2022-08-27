5a4c5da0566a5c4eafbf91f49d6ff5089cda3138
David Lambert <david.lambert@my.utsa.edu>
2022-08-27 09:50:49 -0500

    added signin, signup, signout

--- index.js
+++ index.js
@@ -11,21 +11,21 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieSession({ keys: ['asdfjkl'] }))
 
 //route handler, request and response objects, .get means wait for get request and path of '/'
-app.get('/', (req, res) => {
+app.get('/signup', (req, res) => {
     res.send(`
     <div>
     Your ID is: ${req.session.userId}
         <form method="POST">
             <input name="email" placeholder="email" />
             <input name="password"placeholder="password" />
-            <input name="passwordConfirmation"placeholder="password confirmation" />
+            <input name="passwordConfirmation" placeholder="password confirmation" />
             <button>Sign Up</button>
         </form>
     </div>
     `)
 })
 
-app.post('/', async(req, res) => {
+app.post('/signup', async(req, res) => {
     const { email, password, passwordConfirmation } = req.body
         //check if someone signed up with email before
     const existingUser = await usersRepo.getOneBy({ email: email })
@@ -37,8 +37,42 @@ app.post('/', async(req, res) => {
     }
 
     const user = await usersRepo.create({ email: email, password: password })
-    req.session.userId = user.id //added by cookie-session, attached auto to req
+    req.session.userId = user.id //added by cookie-session, attached auto to req, .session empty obj at first
     res.send("Account created")
+})
+
+app.get('/signout', (req, res) => {
+    //deleting cookie forgets user
+    req.session = null
+    res.send("You are logged out.")
+})
+
+app.get('/signin', (req, res) => {
+    res.send(`
+    <div>
+        <form method="POST">
+            <input name="email" placeholder="email" />
+            <input name="password"placeholder="password" />
+            <button>Sign In</button>
+        </form>
+    </div>
+    `)
+})
+
+app.post('/signin', async(req, res) => {
+    //email and password from names put on form
+    const { email, password } = req.body
+
+    const user = await usersRepo.getOneBy({ email: email })
+
+    if (!user) {
+        return res.send("Email not found.")
+    }
+    if (user.password !== password) {
+        return res.send("Invalid Password")
+    }
+    req.session.userId = user.id
+    res.send("You are signed in")
 })
 
 //listen on port 3000 for request then run callback
