320461a25b6ee3993a8ce2250b7a567cb4ba4023
David Lambert <david.lambert@my.utsa.edu>
2022-09-04 14:03:49 -0500

    done for day #423

--- routes/admin/middlewares.js
+++ routes/admin/middlewares.js
@@ -1,13 +1,14 @@
 const { validationResult } = require("express-validator");
 
 module.exports = {
-  handleErrors(templaceFunc) {
+  handleErrors(templaceFunc, dataCallBack) {
     //middlewear gets next param, it means continue on before promises were around
     //middlewear returns a func
     return (req, res, next) => {
       const errors = validationResult(req);
 
       if (!errors.isEmpty()) {
+        
         return res.send(templaceFunc({ errors }));
       }
       //everything went well, move on to next call
